8b136ab4795d7b5678fed5f05cd334747d9d4beb
gorillaheartmedia <gorillaheartmedia@gmail.com>
2026-01-01 04:50:26 -0500

    Add files via upload

---
+++ assets/dinerfloor.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="dinerfloor" tilewidth="1024" tileheight="1024" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../OneDrive/Desktop/HTML/diner/assets/floorblack_iso.png" width="1024" height="1024"/>
+ </tile>
+ <tile id="1">
+  <image source="../OneDrive/Desktop/HTML/diner/assets/floorwhite_iso.png" width="1024" height="1024"/>
+ </tile>
+</tileset>
